6d803c89c7e99a462d595c71f6c690f567d27250
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-05-02 22:39:20 +0700

    Prisma Client Test

--- test/prisma-client.test.js
+++ test/prisma-client.test.js
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 describe("Prisma Client", () => {
-    it('should be able to connect to database', async () => {
-        const prisma = new PrismaClient();
-        await prisma.$connect();
+  it("should be able to connect to database", async () => {
+    const prisma = new PrismaClient();
+    await prisma.$connect();
 
-        // do something
+    // do something
 
-        await prisma.$disconnect();
-    });
+    await prisma.$disconnect();
+  });
 });
