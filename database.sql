4c1ae3291ab0d0b8ee82cbe9fd41e0b1f584e3d9
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-05-02 22:40:02 +0700

    Create Table Customer

--- database.sql
+++ database.sql
@@ -3,3 +3,12 @@ CREATE TABLE sample (
     name VARCHAR(100) not NULL,
     primary key(id)
 ) engine = InnoDB;
+CREATE TABLE customers (
+    id VARCHAR(100) not NULL,
+    name VARCHAR(100) not NULL,
+    email VARCHAR(100) not NULL,
+    phone VARCHAR(100) not NULL,
+    primary key(id),
+    constraint email_unique UNIQUE (email),
+    constraint phone_unique UNIQUE (phone)
+) engine = InnoDB;
